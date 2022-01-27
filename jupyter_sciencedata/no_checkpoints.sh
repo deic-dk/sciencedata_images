95f570e113b0873c0432cec97b6ea899268eb147
Frederik Orellana <frederik@orellana.dk>
2022-01-27 20:26:32 +0100

    living with checkpoints

--- jupyter_sciencedata/no_checkpoints.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -Ei "s|(checkpoint = yield maybe_future\(cm\.create_checkpoint\(path\)\))|\1\n        if not checkpoint:\n            return|" /opt/conda/lib/python3.8/site-packages/notebook/services/contents/handlers.py
