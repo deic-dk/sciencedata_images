1fd49bbd28f9c83528878529161c4a9d8c292c3d
joshuadclement <joshua.clement@i2.dk>
2022-05-05 22:40:15 +0200

    First attempt at fixing ssh, also provide host keys
    Works in docker, but not in kubernetes.
    Remaining chroot error.

--- ubuntu_focal_sciencedata/start.sh
+++ ubuntu_focal_sciencedata/start.sh
@@ -3,8 +3,8 @@
 # SSH access to root
 if [ -n "$SSH_PUBLIC_KEY" ]; then
 	echo "$SSH_PUBLIC_KEY" >> /root/.ssh/authorized_keys
-else
-	echo "root:$ROOT_PASSWORD" | chpasswd;
+#else
+#	echo "root:$ROOT_PASSWORD" | chpasswd;
 fi
 
 # Resolve sciencedata to the 10.2.0.0/24 address of the silo of the user
@@ -18,8 +18,23 @@ fi
 umask 000
 echo "umask 000" >> ~/.bashrc
 
-service php7.4-fpm start
+###### Begin generate new ssh_host keys
+rm /etc/ssh/ssh_host*
+ssh-keygen -t ed25519 -N '' -f /etc/ssh/ssh_host_ed25519_key
+ssh-keygen -t rsa -N '' -f /etc/ssh/ssh_host_rsa_key
+#get fingerprints
+ed_fprint=$(ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub | sed -E 's|.*SHA256:(.*) root.*|\1|')
+rsa_fprint=$(ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub | sed -E 's|.*SHA256:(.*) root.*|\1|')
+echo \
+'{
+  "ed25519": "EDFINGERPRINT",
+  "rsa": "RSAFINGERPRINT"
+}' > /tmp/hostkeys
+sed -i "s|EDFINGERPRINT|$ed_fprint|" /tmp/hostkeys
+sed -i "s|RSAFINGERPRINT|$rsa_fprint|" /tmp/hostkeys
+###### End generate new ssh_host keys
+
 service cron start
 cd /root
 export HOSTNAME
-/usr/sbin/dropbear -p 22 -W 65536 -F -E
+/usr/sbin/sshd -D
