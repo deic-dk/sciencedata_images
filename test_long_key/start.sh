b4cb759b6e90437a7a83540ce8e04a901af0fc62
joshuadclement <joshua.clement@i2.dk>
2022-09-06 21:56:29 +0200

    Testing images

---
+++ test_long_key/start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo 'my short key' > /tmp/shortkey
+echo 'this key is very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+very very very very very very very very very very very very very very very very very very very very very
+long' > /tmp/longkey
+
+# Run forever while allowing graceful shutdown
+while :; do :; done & kill -STOP $! && wait $!
+
