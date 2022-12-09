1377a4c636f4ae2d76163b1c1848a118f4557155
joshuadclement <joshua.clement@i2.dk>
2022-12-09 18:59:40 +0100

    Nearly working jupyter image

--- new_jupyter_sciencedata/before-notebook.d/copy-token.sh
+++ new_jupyter_sciencedata/before-notebook.d/copy-token.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 for i in {1..60}; do
   sleep 1
   echo $i
   jupyter server list
-  uri=$(jupyter server list | grep -E ' *http://[^/]*/(.*token=.*) *' | \
+  uri=$(sudo -u sciencedata jupyter server list | grep -E ' *http://[^/]*/(.*token=.*) *' | \
     sed -E 's| *http://[^/]*/(.*token=[^ ]*).*|\1|' | tail -1 | awk '{print $1}')
   if [[ -n "$uri" ]]; then
     if [[ -n "$FILE" ]]; then
---
+++ new_jupyter_sciencedata/before-notebook.d/set-hostname.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "$HOME_SERVER_IP    sciencedata" >> /etc/hosts
+echo "$HOME_SERVER_IP    $HOME_SERVER_HOSTNAME" >> /etc/hosts
