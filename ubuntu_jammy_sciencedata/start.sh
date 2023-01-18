f3591e50123b2454a4afb579546cc683c1b198af
joshuadclement <joshua@clement.tel>
2023-01-18 17:16:54 +0100

    Clean up ubuntu, start python_worker

--- ubuntu_jammy_sciencedata/start.sh
+++ ubuntu_jammy_sciencedata/start.sh
@@ -4,20 +4,10 @@
 if [[ -n "$SSH_PUBLIC_KEY" ]]; then
 	echo "$SSH_PUBLIC_KEY" >> /root/.ssh/authorized_keys
 fi
-if [[ -n "$ROOT_PASSWORD" ]]; then
-	echo "root:$ROOT_PASSWORD" | chpasswd;
-fi
 
 # Resolve sciencedata to the 10.2.0.0/24 address of the silo of the user
-[[ -n $HOME_SERVER ]] && echo "$HOME_SERVER	sciencedata" >> /etc/hosts
-[[ -n $HOME_SERVER ]] && echo "*/5 * * * * root grep sciencedata /etc/hosts || echo \"$HOME_SERVER	sciencedata\" >> /etc/hosts" > /etc/cron.d/sciencedata_hosts
-
-# Make everything world read/writeable.
-# This is to allow user www on the home silo to modify/delete files/directories in the
-# directory exported and mounted as www.
-# - we anyway only have one user on this system - root.
-umask 000
-echo "umask 000" >> ~/.bashrc
+echo "$HOME_SERVER_IP    sciencedata" >> /etc/hosts
+echo "$HOME_SERVER_IP    $HOME_SERVER_HOSTNAME" >> /etc/hosts
 
 ###### Begin generate new ssh_host keys
 rm /etc/ssh/ssh_host*
@@ -28,7 +18,5 @@ ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub | sed -E 's|.*(SHA256:.*) roo
 ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub | sed -E 's|.*(SHA256:.*) root.*|\1|' > /tmp/rsaHostKeyHash
 ###### End generate new ssh_host keys
 
-service cron start
 cd /root
-export HOSTNAME
 /usr/sbin/sshd -D
