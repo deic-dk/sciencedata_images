ce31fa0c9681cbed254b77b6cdf09a5181b359af
Frederik Orellana <frederik@orellana.dk>
2026-05-13 00:43:54 +0200

    Version bump, web UI.

--- ubuntu_noble_versitygw_sciencedata/start.sh
+++ ubuntu_noble_versitygw_sciencedata/start.sh
@@ -16,46 +16,58 @@ fi
 
 export HOSTNAME
 
-chown -R s3:s3 /mnt/vgw
-
 ls /mnt/vgw/data 2>/dev/null || mkdir -p /mnt/vgw/data
 ls /mnt/vgw/accounts 2>/dev/null || mkdir -p /mnt/vgw/accounts
 ls /mnt/vgw/versions 2>/dev/null || mkdir -p /mnt/vgw/versions
 mkdir /home/s3/sidecar
+
 if [ ! -s /mnt/vgw/versitygw.conf ]; then
 	secret_key="`openssl rand -base64 16 | md5sum | awk '{print $1}'`"
 	cat <<EOF>>/mnt/vgw/versitygw.conf
-	VGW_BACKEND=posix
-	VGW_BACKEND_ARG=/mnt/vgw/data
-	VGW_VERSIONING_DIR=/mnt/vgw/versions
-	ROOT_ACCESS_KEY_ID=admin
-	ROOT_SECRET_ACCESS_KEY=$secret_key
-	VGW_IAM_DIR=/mnt/vgw/accounts
+export VGW_BACKEND=posix
+export VGW_BACKEND_ARG=/mnt/vgw/data
+export VGW_VERSIONING_DIR=/mnt/vgw/versions
+export ROOT_ACCESS_KEY_ID=admin
+export ROOT_SECRET_ACCESS_KEY=$secret_key
+export VGW_IAM_DIR=/mnt/vgw/accounts
+export AWS_ACCESS_KEY_ID=$ROOT_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$ROOT_SECRET_ACCESS_KEY
 EOF
 fi
+
+# For run_pod to set NFS_VOLUME_PATH requires setting the environment variable NFS_VOLUME_PATH (to "") in the YAML.
+# This may have been forgotten by the YAML author, so we fall back to using df.
+if [ -z "$NFS_VOLUME_PATH" ]; then
+	mount_path=`df | grep :/ | awk '{print $1}'`
+	mount_dir=`basename $mount_path`
+else
+	mount_dir=`basename "$NFS_VOLUME_PATH"`
+fi
+export DST_NAME=versitygw-$mount_dir.tar.gz
 
 function gracefulShutdown {
   echo "Shutting down!"
   killall versitygw
   cd /tmp
   tar -cvzf versitygw.tar.gz versitygw
-  curl --insecure --upload /tmp/versitygw.tar.gz https://sciencedata/files/versitygw.tar.gz
+  curl --insecure --upload /tmp/versitygw.tar.gz https://sciencedata/files/$DST_NAME
 }
 
 cd /tmp
 for i in 1 2 3 4; do
-  status=`curl -I --silent --insecure https://sciencedata/files/versitygw.tar.gz | grep ^HTTP | awk '{print $2}'`
-  [[ $status > 399 ]] && break
-  [[ $status < 400 ]] && curl -LO --insecure https://sciencedata/files/versitygw.tar.gz && tar -xvzf versitygw.tar.gz && break
+  status=`curl -I --silent --insecure https://sciencedata/files/$DST_NAME | grep ^HTTP | awk '{print $2}'`
+  [[ -n "$status" && $status < 400 ]] && curl -L -o versitygw.tar.gz --insecure https://sciencedata/files/$DST_NAME && tar -xvzf versitygw.tar.gz && break
   sleep 10
 done
 
 ls /tmp/versitygw 2>/dev/null || mkdir /tmp/versitygw
 
+chown -R s3:s3 /mnt/vgw /home/s3/sidecar /tmp/versitygw
+
 trap gracefulShutdown EXIT
 set -a
 source /mnt/vgw/versitygw.conf
 set +a
-exec versitygw $VGW_BACKEND --sidecar /tmp/versitygw $VGW_BACKEND_ARG &
+sudo -E -u s3 versitygw --webui :8080 $VGW_BACKEND --sidecar /tmp/versitygw $VGW_BACKEND_ARG &
 
 /usr/sbin/dropbear -p 22 -W 65536 -F -E
